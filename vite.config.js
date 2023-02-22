35f6fba44ebed4d12c92b1d2006c1f3708d050c4
yuche <26004836+YunosukeYoshino@users.noreply.github.com>
2023-02-22 21:45:06 +0900

    🔥 設定ファイルの修正

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
-const { resolve } = require('path')
+import fs from 'fs';
 
-const root = 'src'
+const { resolve } = require('path');
+
+const root = './src';
+
+/*
+* htmlファイルのみ抽出
+*/
+const fileNameList = fs.readdirSync(resolve(__dirname, './src/'));
+
+const htmlFileList = fileNameList.filter(file => /.html$/.test(file));
+
+const inputFiles = {};
+for (let i = 0; i < htmlFileList.length; i++) {
+  const file = htmlFileList[i]; inputFiles[file.slice(0, -5)] = resolve(__dirname, './src/' + file);
+}
+
+
 module.exports = {
   root: root,
   publicDir: 'public',
@@ -8,18 +24,31 @@ module.exports = {
   base: "./",
   build: {
     outDir: '../dist',
+    emptyOutDir: true,
+    //出力場所の指定
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, root, 'index.html'),
-        privacy: resolve(__dirname, root, 'privacy/index.html'),
-
-
-      },
+      //ファイル出力設定
       output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
-      }
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          //Webフォントファイルの振り分け
+          if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
+            extType = 'fonts';
+          }
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'images';
+          }
+          //ビルド時のCSS名を明記してコントロールする
+          if (extType === 'css') {
+            return `assets/css/style.css`;
+          }
+          return `assets/${extType}/[name][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name].js',
+        entryFileNames: 'assets/js/[name].js',
+      },
+      input: inputFiles,
+
     }
   }
-}
+};
