3f5aa0f6d47df856935114180a7f9f5ec89168b0
yuche <26004836+YunosukeYoshino@users.noreply.github.com>
2023-02-24 17:45:26 +0900

    ⚡️ 設定ファイルの更新

--- vite.config.js
+++ vite.config.js
@@ -1,54 +1,92 @@
-import fs from 'fs';
+import fs from "fs";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import cleanPlugin from "vite-plugin-clean";
+import handlebars from "vite-plugin-handlebars";
+import viteImagemin from "vite-plugin-imagemin";
 
-const { resolve } = require('path');
-
-const root = './src';
+// 各ページのメタデータを読み込む
+const metadata = JSON.parse(fs.readFileSync(resolve(__dirname, "./src/metadata.json")));
 
 /*
-* htmlファイルのみ抽出
-*/
-const fileNameList = fs.readdirSync(resolve(__dirname, './src/'));
+ * htmlファイルのみ抽出
+ */
 
-const htmlFileList = fileNameList.filter(file => /.html$/.test(file));
+// HTMLの複数出力を自動化する
+//./src配下のファイル一式を取得
+const fileNameList = fs.readdirSync(resolve(__dirname, "./src/"));
+
+//htmlファイルのみ抽出
+const htmlFileList = fileNameList.filter((file) => /.html$/.test(file));
 
 const inputFiles = {};
 for (let i = 0; i < htmlFileList.length; i++) {
-  const file = htmlFileList[i]; inputFiles[file.slice(0, -5)] = resolve(__dirname, './src/' + file);
+	const file = htmlFileList[i];
+	inputFiles[file.slice(0, -5)] = resolve(__dirname, "./src/" + file);
 }
 
-
-module.exports = {
-  root: root,
-  publicDir: 'public',
-  publicPath: './',
-  base: "./",
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    //出力場所の指定
-    rollupOptions: {
-      //ファイル出力設定
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.')[1];
-          //Webフォントファイルの振り分け
-          if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
-            extType = 'fonts';
-          }
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'images';
-          }
-          //ビルド時のCSS名を明記してコントロールする
-          if (extType === 'css') {
-            return `assets/css/style.css`;
-          }
-          return `assets/${extType}/[name][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name].js',
-        entryFileNames: 'assets/js/[name].js',
-      },
-      input: inputFiles,
-
-    }
-  }
-};
+export default defineConfig({
+	server: {
+		host: true, //IPアドレスを有効化
+	},
+	root: "./src", //開発ディレクトリ設定
+	emptyOutDir: true,
+	publicDir: "public",
+	publicPath: "./",
+	base: "./",
+	build: {
+		outDir: "../dist",
+		//出力場所の指定
+		rollupOptions: {
+			//ファイル出力設定
+			output: {
+				assetFileNames: (assetInfo) => {
+					let extType = assetInfo.name.split(".")[1];
+					//Webフォントファイルの振り分け
+					if (/ttf|otf|eot|woff|woff2/i.test(extType)) {
+						extType = "fonts";
+					}
+					if (/png|jpe?g|svg|webp|avif|gif|tiff|bmp|ico/i.test(extType)) {
+						extType = "img";
+					}
+					return `assets/${extType}/[name][extname]`;
+				},
+				chunkFileNames: "assets/js/[name].js",
+				entryFileNames: "assets/js/[name].js",
+			},
+			input: inputFiles,
+		},
+	},
+	plugins: [
+		cleanPlugin(), //build時にファイルを削除
+		handlebars({
+			//コンポーネントの格納ディレクトリを指定
+			partialDirectory: resolve(__dirname, "./src/html-components"),
+			//各ページ情報の読み込み
+			context(pagePath) {
+				return metadata[pagePath];
+			},
+		}),
+		viteImagemin({
+			// 画像圧縮の設定
+			gifsicle: {
+				optimizationLevel: 3,
+			},
+			mozjpeg: {
+				quality: 65,
+				progressive: true,
+			},
+			pngquant: {
+				quality: [0.65, 0.8],
+				speed: 1,
+			},
+			svgo: {
+				plugins: [
+					{
+						name: "removeViewBox",
+					},
+				],
+			},
+		}),
+	],
+});
