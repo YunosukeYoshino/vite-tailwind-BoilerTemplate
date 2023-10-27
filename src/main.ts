26631dcc127ab629e8661b0c0a0b24d4e47cd6ff
YOSHINO <26004836+YunosukeYoshino@users.noreply.github.com>
2023-10-27 17:22:31 +0900

    ✨ TypeScript化しました。

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import "./css/main.css";
 import "./scss/style.scss";
 
-document.querySelector("#app").innerHTML = `
+const app = document.querySelector("#app") as HTMLElement;
+
+app.innerHTML = `
   <h1 class="text-2xl text-red-500">Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
